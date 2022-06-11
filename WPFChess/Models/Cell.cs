a96c5c91f72fde1da346903ac1471ea4b6c9af8d
Alex Rajvandary <stark.11raj@gmail>
2022-06-12 01:38:21 +0300

    отсортировал файлы по папкам

--- WPFChess/Models/Cell.cs
+++ WPFChess/Models/Cell.cs
@@ -7,6 +7,12 @@
     {
         private State _state;
         private bool _active;
+
+        public Cell(int horizontal, int vertical)
+        {
+            Position = new Position(horizontal, vertical);
+        }
+
         /// <summary>
         /// Позиция клетки на игровой доске описывается двумя натуральными числами (по горизонтали, по вертикали)
         /// </summary>
@@ -36,37 +42,6 @@
                 _active = value;
                 OnPropertyChanged();
             }
-        }
-        public Cell(int horizontal, int vertical)
-        {
-            Position = new Position(horizontal, vertical);
-        }
-    }
-    /// <summary>
-    /// Позиция 
-    /// </summary>
-    public class Position
-    {
-        /// <summary>
-        /// Позиция по вертикали
-        /// </summary>
-        public int Vertical { get; set; }
-        /// <summary>
-        /// Позиция по горизонтали
-        /// </summary>
-        public int Horizontal { get; set; }
-        /// <summary>
-        /// Текстовое представление шахматной позиции
-        /// </summary>
-        /// <returns></returns>
-        public override string ToString()
-        {
-            return $"{"ABCDEFGH"[Horizontal]}{Vertical + 1}";
-        }
-        public Position(int Horizontal, int Vertical)
-        {
-            this.Horizontal = Horizontal;
-            this.Vertical = Vertical;
         }
     }
 }
---
+++ WPFChess/Models/Position.cs
@@ -0,0 +1,30 @@
+﻿namespace ChessBoard
+{
+    /// <summary>
+    /// Позиция 
+    /// </summary>
+    public class Position
+    {
+        /// <summary>
+        /// Позиция по вертикали
+        /// </summary>
+        public int Vertical { get; set; }
+        /// <summary>
+        /// Позиция по горизонтали
+        /// </summary>
+        public int Horizontal { get; set; }
+        /// <summary>
+        /// Текстовое представление шахматной позиции
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return $"{"ABCDEFGH"[Horizontal]}{Vertical + 1}";
+        }
+        public Position(int Horizontal, int Vertical)
+        {
+            this.Horizontal = Horizontal;
+            this.Vertical = Vertical;
+        }
+    }
+}
